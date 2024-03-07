faf26321d1f69d568c6fbe0d40b1e574d7c4ee93
Arttu <arttu.kookoss@gmail.com>
2024-03-07 09:13:50 +0200

    "Kirjaudu ulos" now clears local storage

--- arttukauppi/ryhmä/Green Special Free Website Template - Free-CSS.com (1)/green-special/js/gr.js
+++ arttukauppi/ryhmä/Green Special Free Website Template - Free-CSS.com (1)/green-special/js/gr.js
@@ -5,14 +5,17 @@ if(localStorage.getItem("kirjautunut") === 'kylla') {
 
     document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
     document.getElementById("kirjautumis_lomake").style.display ="none";
+    document.getElementById("menu_nappi").style.display ='block';
+    document.getElementById().textContent += localStorage.getItem("password");
+    }
 
-}
-
+            
 }
 function kirjaudu() {
     
     localStorage.setItem("nimi",document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut","kylla");
+    localStorage.setItem("salasana",document.getElementById("salasana").value);
 }
 
 if (kirjautunut) {
@@ -20,12 +23,19 @@ if (kirjautunut) {
 } else {
     kirjauduUlosNappi.style.display = 'none';
 }
-
+if (kirjautunut) {
+    menu_nappi.style.display = 'block';
+} else {
+    menu_nappi.style.display = 'none';
+}
 
 function kirjauduUlos() {
-localStorage.removeItem('kirjautunut');
-tarkistaKirjautuminen(); 
+    localStorage.clear();
+    tarkistaKirjautuminen(); 
 }
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
 tarkistaKirjautuminen();
